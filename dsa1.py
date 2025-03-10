602470c65cfd9d30c9abd789a100796140ae9ffa
Vitória <vitoriamaglio@gmail.com>
2025-03-10 18:37:01 -0300

    dia01_python

--- dsa1.py
+++ dsa1.py
@@ -4,3 +4,11 @@ print("Olá Mundo!")
 n_real = 5.75  # Um número real (float)
 n_decimal = int(n_real)  # Convertendo de real para inteiro (removendo a parte decimal)
 print(n_decimal)  # A saída será 5
+
+var=9.5
+print(type(var))
+
+s="Data science"
+print(s)
+print(s[0])
+print(s[1])
