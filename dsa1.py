58602082092cf8e78db7c72a21a20f0744f6bf7b
Vitória <vitoriamaglio@gmail.com>
2025-03-10 16:56:09 -0300

    for

---
+++ dsa1.py
@@ -0,0 +1,6 @@
+# Exemplo de código
+print("Olá Mundo!")
+
+n_real = 5.75  # Um número real (float)
+n_decimal = int(n_real)  # Convertendo de real para inteiro (removendo a parte decimal)
+print(n_decimal)  # A saída será 5
