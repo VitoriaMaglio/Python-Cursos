4da06003fa0350ded4605366a2ef97c5124aaebe
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-03-11 11:05:13 -0300

    Create dsa01.py

---
+++ dsa01.py
@@ -0,0 +1,14 @@
+# Exemplo de código
+print("Olá Mundo!")
+
+n_real = 5.75  # Um número real (float)
+n_decimal = int(n_real)  # Convertendo de real para inteiro (removendo a parte decimal)
+print(n_decimal)  # A saída será 5
+
+var=9.5
+print(type(var))
+
+s="Data science"
+print(s)
+print(s[0])
+print(s[1])
